7cb7e92510505fe9381860038fae8f2dcd525264
Rahul Janardhanan <62405806+rjanardh@users.noreply.github.com>
2020-08-02 17:19:47 -0700

    Add files via upload

---
+++ hw6.m
@@ -0,0 +1,38 @@
+
+%code to plot BezCurve
+points_number_input=input('How many points?  ');
+
+[Coordinates_input]=input('Enter Co-ordinates in array form ');
+ 
+n_p = points_number_input -1;    
+    
+for    i=0:1:n_p
+
+    combination(i+1)= factorial(n_p)/(factorial(i)*factorial(n_p-i));   
+
+end
+
+Mat_fin=[];
+    
+ Mat_emp=[];
+
+ for k=0:0.01:1 %filling in mat values
+     
+    for i=1:points_number_input
+        
+        Mat_emp(i)=combination(i)*((1-k)^(points_number_input-i))*(k^(i-1)); %recursive def of bezier curve
+    end
+    
+            Mat_fin=cat(1,Mat_fin,Mat_emp); %combining matrices
+            
+                end
+Mat_new=Mat_fin*Coordinates_input;
+
+    line(Mat_new(:,1),Mat_new(:,2),Mat_new(:,3))
+
+    line(Coordinates_input(:,1),Coordinates_input(:,2),Coordinates_input(:,3))
+    
+    
+    
+
+    
